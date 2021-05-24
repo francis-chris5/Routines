7b9100a7afc9a5de5158a8938063d9a1fcc1df9f
francis-chris5 <50467171+francis-chris5@users.noreply.github.com>
2021-05-24 02:27:08 -0400

    Delete Routines/dist directory

--- Routines/dist/Routines-Windows_Launcher.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-
-
-java --module-path %cd%\javafx-sdk-11.0.2\lib --add-modules javafx.controls,javafx.fxml,javafx.web --add-opens=javafx.graphics/javafx.scene=ALL-UNNAMED --add-exports javafx.base/com.sun.javafx.event=ALL-UNNAMED -jar Routines.jar
-
