fa53ebc265aca9d251dacc145b3c67e4db49a4f2
francis-chris5 <50467171+francis-chris5@users.noreply.github.com>
2021-05-23 20:08:29 -0400

    Add files via upload

--- Routines/src/Routines/ResourceUsageChart.java
+++ Routines/src/Routines/ResourceUsageChart.java
@@ -2,20 +2,15 @@
 package Routines;
 
 
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.Comparator;
+import java.util.LinkedList;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Side;
-import javafx.scene.chart.BubbleChart;
 import javafx.scene.chart.CategoryAxis;
-import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.StackedBarChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 
 
 public class ResourceUsageChart extends Pane{
@@ -25,11 +20,8 @@ public class ResourceUsageChart extends Pane{
     //private CategoryAxis yAxis = new CategoryAxis();
     private CategoryAxis xAxis = new CategoryAxis();
     private NumberAxis yAxis = new NumberAxis();
-    private NumberAxis zAxis = new NumberAxis();
-    private LineChart<String, Number> chart = new LineChart<>(xAxis, yAxis);
-    private ObservableList<XYChart.Series> taskList = FXCollections.observableArrayList();
     private ObservableList<XYChart.Series> resourceList = FXCollections.observableArrayList();
-    private StackedBarChart<String, Number> chart2 = new StackedBarChart(xAxis, zAxis);
+    private StackedBarChart<String, Number> chart = new StackedBarChart(xAxis, yAxis);
     
     
     
@@ -40,21 +32,13 @@ public class ResourceUsageChart extends Pane{
     
     public ResourceUsageChart(Routine routine){
         chart.setTitle(routine.getRoutineName() + " Resource Usage Chart");
-        chart.setTitle(routine.getRoutineName() + " Resource Usage Stacked Bar Chart");
         xAxis.setSide(Side.TOP);
         yAxis.setTickLabelsVisible(false);
         setData(routine);
-        //chart.setPrefHeight(taskList.size()*22 + 250);
-        for(int i = 0; i < taskList.size(); i++){
-            chart.getData().add(taskList.get(i));
-        }
         for(int i = 0; i < resourceList.size(); i++){
-            chart2.getData().add(resourceList.get(i));
+            chart.getData().add(resourceList.get(i));
         }
-        chart.setCreateSymbols(false);
-        chart.setLegendSide(Side.TOP);
-        VBox vbxCharts = new VBox(chart, chart2);
-        this.getChildren().addAll(vbxCharts);
+        this.getChildren().add(chart);
     }//end default-constructor
     
     
@@ -67,20 +51,17 @@ public class ResourceUsageChart extends Pane{
         ////////////////////////////////////////////  CHART METHODS  /////////
     
     public void setData(Routine routine){
-        for(int i=0; i < routine.getAvailableResources().size(); i++){
-            XYChart.Series data = new XYChart.Series();
-            
-            for(int j = 0; j < routine.getAvailableResources().get(i).getAssignedTo().size(); j++){
-                data.getData().add(new XYChart.Data(routine.getAvailableResources().get(i).getAssignedTo().get(j).getName(), -i-1));
-                data.setName(routine.getAvailableResources().get(i).getName());
-                XYChart.Series data2 = new XYChart.Series();
-                data2.getData().add(new XYChart.Data(routine.getAvailableResources().get(i).getName(), routine.getAvailableResources().get(i).getAssignedTo().get(j).getDuration()));
-                data2.setName(routine.getAvailableResources().get(i).getAssignedTo().get(j).getName());
-                resourceList.add(data2);
-            }
-            taskList.add(data);
-            
+        LinkedList<XYChart.Series> dataList = new LinkedList<>();
+        for(int i = 0; i < routine.getRoutineTasks().size(); i++){
+            dataList.add(new XYChart.Series());
+            dataList.get(i).setName(routine.getRoutineTasks().get(i).getName());
         }
+        for(int i = 0; i < routine.getRoutineTasks().size(); i++){
+            for(int j = 0; j < routine.getRoutineTasks().get(i).getAssignedResources().size(); j++){
+                dataList.get(i).getData().add(new XYChart.Data(routine.getRoutineTasks().get(i).getAssignedResources().get(j).getName(), routine.getRoutineTasks().get(i).getDuration()));
+            }
+        }
+        resourceList.addAll(dataList);
     }//end setData()
     
 }//end ResourceUsageChart
