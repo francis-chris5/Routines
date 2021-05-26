7838f4f087aaa571bd302960c1f7ad62a5c4148a
francis-chris5 <50467171+francis-chris5@users.noreply.github.com>
2021-05-25 21:03:08 -0400

    Add files via upload

--- Routines/dist/Routines-Launcher.bat
+++ Routines/dist/Routines-Launcher.bat
@@ -1,13 +1,12 @@
 @ECHO OFF
 
 IF NOT EXIST javafx\ (
-
+	echo Need to download some dependencies first...
 	curl https://download2.gluonhq.com/openjfx/11.0.2/openjfx-11.0.2_windows-x64_bin-sdk.zip --output javafx.zip
 
 
 	Powershell.exe Expand-Archive javafx.zip
 )
 
-
+echo Opening Routines Application...
 java --module-path "%cd%\javafx\javafx-sdk-11.0.2\lib" --add-modules javafx.controls,javafx.fxml,javafx.web --add-opens=javafx.graphics/javafx.scene=ALL-UNNAMED --add-exports javafx.base/com.sun.javafx.event=ALL-UNNAMED -jar Routines.jar
-
