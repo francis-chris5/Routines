6ad6fa8453b4256592df6dc428e4d93a4b5031a8
francis-chris5 <50467171+francis-chris5@users.noreply.github.com>
2021-05-27 00:16:50 -0400

    Add files via upload

---
+++ Routines/Routines-Launcher.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+echo
+if [ ! -d javafx-sdk-11.0.2 ] 
+then
+	echo Need to download some dependencies first...
+	echo
+	wget https://download2.gluonhq.com/openjfx/11.0.2/openjfx-11.0.2_linux-x64_bin-sdk.zip > temp.txt
+	unzip openjfx-11.0.2_linux-x64_bin-sdk.zip > temp.txt
+	rm openjfx-11.0.2_linux-x64_bin-sdk.zip > temp.txt
+	rm temp.txt
+	echo
+fi
+
+
+echo Opening Routines Application...
+echo
+current=$(pwd)
+java -cp $current/bin --module-path $current/javafx-sdk-11.0.2/lib --add-modules javafx.controls,javafx.fxml,javafx.web Routines.RoutineMain
+
